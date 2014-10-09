353f5be0ccbf58400eb3a102488e1d1a64dd377f
RuDas-KR <jjm1411@naver.com>
2014-10-09 17:09:22 +0900

    BCD

---
+++ CodeConverter/BinarytoBCD/binarytobcd.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+---- Uncomment the following library declaration if instantiating
+---- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity binarytobcd is
+	port	(	binary_in : in STD_LOGIC_VECTOR (3 DOWNTO 0);
+				BCD_out : out STD_LOGIC_VECTOR (5 DOWNTO 0)
+				);
+end binarytobcd;
+
+architecture Behavioral of binarytobcd is
+
+begin
+	process (binary_in)
+	begin
+		case (binary_in) is
+				when "0000" => BCD_out <= "000000";
+				when "0001" => BCD_out <= "000001";
+				when "0010" => BCD_out <= "000010";
+				when "0011" => BCD_out <= "000011";
+				when "0100" => BCD_out <= "000100";
+				when "0101" => BCD_out <= "000101";
+				when "0110" => BCD_out <= "000110";
+				when "0111" => BCD_out <= "000111";
+				when "1000" => BCD_out <= "001000"; -- 8
+				when "1001" => BCD_out <= "001001"; -- 9
+				when "1010" => BCD_out <= "010000"; -- 10
+				when others => BCD_out <= "010001"; -- 11
+		end case;
+		
+	end process;
+
+end Behavioral;
+
---
+++ CodeConverter/BinarytoBCD/tb_binarytobcd.vhd
@@ -0,0 +1,58 @@
+
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_unsigned.all;
+USE ieee.numeric_std.ALL;
+ 
+ENTITY tb_binarytobcd IS
+END tb_binarytobcd;
+ 
+ARCHITECTURE behavior OF tb_binarytobcd IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT binarytobcd
+    PORT(
+         binary_in : IN  std_logic_vector(3 downto 0);
+         BCD_out : OUT  std_logic_vector(5 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal binary_in : std_logic_vector(3 downto 0) := (others => '0');
+
+ 	--Outputs
+   signal BCD_out : std_logic_vector(5 downto 0);
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: binarytobcd PORT MAP (
+          binary_in => binary_in,
+          BCD_out => BCD_out
+        );
+ 
+   -- No clocks detected in port list. Replace <clock> below with 
+   -- appropriate port name 
+ 
+   tb : process
+	begin
+	
+		binary_in <= "0000"; wait for 100 ns;
+		binary_in <= "0001"; wait for 100 ns;
+		binary_in <= "0010"; wait for 100 ns;
+		binary_in <= "0011"; wait for 100 ns;
+		binary_in <= "0100"; wait for 100 ns;
+		binary_in <= "0101"; wait for 100 ns;
+		binary_in <= "0110"; wait for 100 ns;
+		binary_in <= "0111"; wait for 100 ns;
+		binary_in <= "1000"; wait for 100 ns;
+		binary_in <= "1001"; wait for 100 ns;
+		binary_in <= "1010"; wait for 100 ns;
+		binary_in <= "1011"; wait for 100 ns;
+	
+   end process;
+
+END;
