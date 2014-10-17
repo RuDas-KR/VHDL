768215525cb42603a09fbe99bb1d75f036b0fd3a
RuDas-KR <jjm1411@naver.com>
2014-10-17 18:09:35 +0900

    adjust 7segment

--- CodeConverter/BCDto7Segment/bcd_7seg.vhd
+++ CodeConverter/BCDto7Segment/bcd_7seg.vhd
@@ -5,35 +5,34 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity bcd_7seg is
     Port ( bcd : in  STD_LOGIC_VECTOR (7 DOWNTO 0);
-			  digit : out STD_LOGIC_VECTOR (6DOWNTO 1);
-           s7segment : out  STD_LOGIC_VECTOR (7 DOWNTO 0)
+	        digit : out STD_LOGIC_VECTOR (6 DOWNTO 1);
+			  s7segment : out  STD_LOGIC_VECTOR (7 DOWNTO 0)
 			  );
 end bcd_7seg;
 
 architecture Behavioral of bcd_7seg is
 
 begin
-	digit <=B"00_0001";
-	process(bcd)
-	begin
-		if		(bcd="00000000") then s7segment <=b"0011_1111";
-		elsif (bcd="00000001") then s7segment <=b"0000_0110"; 
-		elsif (bcd="00000010") then s7segment <=b"0101_1011"; 
-		elsif (bcd="00000011") then s7segment <=b"0100_1111"; 
-		elsif (bcd="00000100") then s7segment <=b"0110_0110"; 
-		elsif (bcd="00000101") then s7segment <=b"0110_1101"; 
-		elsif (bcd="00000110") then s7segment <=b"0111_1101"; 
-		elsif (bcd="00000111") then s7segment <=b"0000_0111"; 
-		elsif (bcd="00001000") then s7segment <=b"0111_1111"; 
-		elsif (bcd="00001001") then s7segment <=b"0110_0111"; 
-		
-		elsif (bcd="00010000") then s7segment <=b"0101_1111"; 
-		elsif (bcd="00010001") then s7segment <=b"0111_1100"; 
-		elsif (bcd="00010010") then s7segment <=b"0011_1001"; 
-		elsif (bcd="00010011") then s7segment <=b"0101_1110"; 
-		elsif (bcd="00010100") then s7segment <=b"0111_1011"; 
-		elsif (bcd="00010101") then s7segment <=b"0111_0001"; 
-		else s7segment<=b"0000_0000";
-		end if;
-	end process;
+		digit <=B"10_0000";
+		process(bcd)
+		begin
+				if  (bcd="00000000") then s7segment <="00111111";
+				elsif (bcd="00000001") then s7segment <="00000110"; 
+				elsif (bcd="00000010") then s7segment <="01011011"; 
+				elsif (bcd="00000011") then s7segment <="01001111"; 
+				elsif (bcd="00000100") then s7segment <="01100110"; 
+				elsif (bcd="00000101") then s7segment <="01101101"; 
+				elsif (bcd="00000110") then s7segment <="01111101"; 
+				elsif (bcd="00000111") then s7segment <="00000111"; 
+				elsif (bcd="00001000") then s7segment <="01111111"; 
+				elsif (bcd="00001001") then s7segment <="01100111";  
+				elsif (bcd="00010000") then s7segment <="01110111"; --A
+				elsif (bcd="00010001") then s7segment <="01111100"; --B
+				elsif (bcd="00010010") then s7segment <="00111001"; --C
+				elsif (bcd="00010011") then s7segment <="01011110"; --D
+				elsif (bcd="00010100") then s7segment <="01111001"; --E
+				elsif (bcd="00010101") then s7segment <="01110001"; --F
+				else s7segment<="00000000";
+				end if;
+		end process;
 end Behavioral;
